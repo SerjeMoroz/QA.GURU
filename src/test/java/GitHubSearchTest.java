cd1481b6b7aa1a1cd22643349ac69054b1dea0ee
Sergei Moroz <moroz.sergei94@gmail.com>
2025-05-07 22:19:35 +0300

    тест на гитхаб

---
+++ src/test/java/GitHubSearchTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class GitHubSearchTest {
+    @Test
+    void githubTest() {
+        open("https://github.com");
+        getWebDriver().manage().window().maximize();
+        $("span[class='flex-1']").click();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+        $$(".Box-sc-g0xbh4-0.JcuiZ").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        sleep(2000);
+
+    }
+}

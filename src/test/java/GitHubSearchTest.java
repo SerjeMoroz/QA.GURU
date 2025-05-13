43bce7b298680058e8037aad3d4e538cdf37261a
Sergei Moroz <moroz.sergei94@gmail.com>
2025-05-13 22:41:05 +0300

    тест на гитхаб c нужным разрешением

--- src/test/java/GitHubSearchTest.java
+++ src/test/java/GitHubSearchTest.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
@@ -16,5 +17,8 @@ public class GitHubSearchTest {
         $("#query-builder-test").setValue("Selenide").pressEnter();
         $$(".Box-sc-g0xbh4-0.JcuiZ").first().$("a").click();
         $("#repository-container-header").shouldHave(text("selenide / selenide"));
+        $("#wiki-tab").click();
+        $(byText("Soft assertions")).shouldHave(text("Soft assertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
